c3d21faf87ae9d2703cf90c6d908f61fbe5c15a6
Andrew <asb104@txstate.edu>
2019-10-09 21:23:23 -0500

    Change to tab2
    Added Map to tab2

--- myApp/src/app/tab2/tab2.page.ts
+++ myApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var google;
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
+export class Tab2Page implements OnInit, AfterContentInit{
+  map;
+  
+  @ViewChild('mapElement', { static: true }) mapElement: ElementRef<HTMLDivElement>; 
   constructor() {}
 
+  ngOnInit(): void{ 
+  }
+
+  ngAfterContentInit(): void {
+    this.map = new google.maps.Map(
+      this.mapElement.nativeElement,{
+    center: {lat: 30.2672, lng: -97.7431},
+    zoom: 8
+
+    });
+
+  }
 }
