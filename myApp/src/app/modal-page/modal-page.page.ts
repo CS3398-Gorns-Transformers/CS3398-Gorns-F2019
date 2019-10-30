4ff1c1b3a3ee0c60cc97ea4ae4d4854ed8f17b95
Andrew.baker <shakenbaker242@gmail.com>
2019-10-30 00:17:58 -0500

    Theme added to app and fixed Google maps to not be in tab 2

--- myApp/src/app/modal-page/modal-page.page.ts
+++ myApp/src/app/modal-page/modal-page.page.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild, ElementRef } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+
+declare var google;
 
 @Component({
   selector: 'app-modal-page',
   templateUrl: './modal-page.page.html',
   styleUrls: ['./modal-page.page.scss'],
 })
-export class ModalPagePage implements OnInit {
+export class ModalPagePage implements OnInit, AfterContentInit {
 
   constructor(public modalController: ModalController) { }
+  map;
+  @ViewChild('mapElement', { static: true }) mapElement: ElementRef<HTMLDivElement>; 
 
-  ngOnInit() {
+  ngOnInit(): void{ 
   }
+  ngAfterContentInit(): void {
+    this.map = new google.maps.Map(
+      this.mapElement.nativeElement,{
+    center: {lat: 30.2672, lng: -97.7431},
+    zoom: 8
+
+    });
+  }
+  
+  
   dismiss(){
 
     this.modalController.dismiss();
