878dc39b70570c8e12e3ee8aa7040f5c7f5f729b
Andrew <asb104@txstate.edu>
2019-10-31 17:04:11 -0500

    Added UI and Weather API
    Added Weather API finally working.. gave it a little UI super basic

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -9,18 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ServicesService} from './weatherService/services.service';
-import {HttpClientModule} from '@angular/common/http'
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
     ServicesService,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Geolocation
   ],
   bootstrap: [AppComponent]
 })
--- myApp/src/app/tab3/tab3.page.ts
+++ myApp/src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+import {Platform} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  place:string="";
+  type:string="";
+  icon:string="";
+  temperature:string="";
+  constructor(public httpClient:HttpClient, public geolocation:Geolocation,
+    public platform : Platform)
+   {
+    this.platform.ready().then(()=>{
+      this.GetCurrentLocation();
+    })
 
-  constructor() {}
-
+   }
+  GetCurrentLocation()
+  {
+  this.geolocation.getCurrentPosition().then((position)=>{
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  this.GetCurrentTemperature(latitude, longitude);
+})
+  }
+  GetCurrentTemperature(latitude, longitude)
+  {
+    var url = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=6fb4b94a9daebbd864d11310ef52c584";
+    this.httpClient.get(url).subscribe((temperaturedate)=>{
+        var obj = <any>temperaturedate;
+        this.place = obj.name;
+        this.type = obj.weather[0].main;
+        this.icon = "https://openweathermap.org/img/w/"+obj.weather[0].icon+".png";
+        this.temperature = ((parseFloat(obj.main.temp)-273.15).toFixed(2)).toString()+"°C";
+    })
+  }
 }
