236d982f263e15e38d336480e11a868ed1c0d022
Andrew.baker <shakenbaker242@gmail.com>
2019-11-10 20:21:36 -0600

    pull this commit

--- myApp/src/app/modal-page/modal-page.page.ts
+++ myApp/src/app/modal-page/modal-page.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterContentInit, ViewChild, ElementRef } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 declare var google;
 
@@ -9,20 +10,70 @@ declare var google;
   styleUrls: ['./modal-page.page.scss'],
 })
 export class ModalPagePage implements OnInit, AfterContentInit {
-
-  constructor(public modalController: ModalController) { }
+  latitude: any;
+  longitude: any;
+  constructor(public modalController: ModalController, private geolocation: Geolocation) { }
   map;
-  @ViewChild('mapElement', { static: true }) mapElement: ElementRef<HTMLDivElement>; 
+  @ViewChild('mapElement', { static: true }) mapNativeElement: ElementRef<HTMLDivElement>; 
 
   ngOnInit(): void{ 
   }
   ngAfterContentInit(): void {
-    this.map = new google.maps.Map(
-      this.mapElement.nativeElement,{
-    center: {lat: 30.2672, lng: -97.7431},
-    zoom: 8
-
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+      const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 16
+      });
+      /*location object*/
+      const pos = {
+        lat: this.latitude - 0,
+        lng: this.longitude
+      };
+      map.setCenter(pos);
+      const icon = {
+        url: 'assets/CurrentLocation.jpg', // image url
+        scaledSize: new google.maps.Size(15, 15), // scaled size
+      };
+      const marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: 'Hello World!',
+        icon: icon
+      });
+      const contentString = '<div id="content">' + 
+          '<div id="siteNotice">' +
+          '</div>' +
+          '<h1 id="firstHeading" class="firstHeading">Current Location</h1>' +
+          '<div id="bodyContent">' +
+          '<img src="assets/icon/user.png" width="200">' +
+          '<p><b>Current Location</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+          'sandstone rock formation in the southern part of the ' +
+          'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+          'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+          '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+          'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+          'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+          'Aboriginal people of the area. It has many springs, waterholes, ' +
+          'rock caves and ancient paintings. Uluru is listed as a World ' +
+          'Heritage Site.</p>' +
+          '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+          'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+          '(last visited June 22, 2009).</p>' +
+          '</div>' +
+          '</div>';
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 400
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+    }).catch((error) => {
+      console.log('Error getting location', error);
     });
+  
   }
   
   
